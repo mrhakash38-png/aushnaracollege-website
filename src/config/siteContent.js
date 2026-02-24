bbdfd2a8b9bd557a6e994222ee628d958ef0ffc3
mrhakash <261758275+mrhakash38-png@users.noreply.github.com>
2026-02-25 00:27:54 +0600

    Add site content config for Aushnara College
    Create src/config/siteContent.js exporting centralized static content for the site: SITE_INFO, CONTACT_INFO, SOCIAL_LINKS, STUDENT_COUNTS, and ADMISSIONS_CYCLE. Includes address, phone/email contacts, social URLs, enrollment metrics, and admissions timeline to be used across the site (homepage, footer, admissions pages).

---
+++ src/config/siteContent.js
@@ -0,0 +1,52 @@
+export const SITE_INFO = {
+  name: 'Aushnara College',
+  domain: 'https://aushnaracollege.edu.bd',
+  establishedYear: 2001
+}
+
+export const CONTACT_INFO = {
+  addressLine1: '23/A, Dhanmondi R/A',
+  addressLine2: 'Dhaka-1205, Bangladesh',
+  mainPhoneDisplay: '+880-2-58154892',
+  mainPhoneUri: '+880258154892',
+  admissionsPhoneDisplay: '+880-2-58154893',
+  admissionsPhoneUri: '+880258154893',
+  academicPhoneDisplay: '+880-2-58154894',
+  academicPhoneUri: '+880258154894',
+  studentAffairsPhoneDisplay: '+880-2-58154895',
+  studentAffairsPhoneUri: '+880258154895',
+  infoEmail: 'info@aushnaracollege.edu.bd',
+  admissionsEmail: 'admissions@aushnaracollege.edu.bd',
+  academicEmail: 'academic@aushnaracollege.edu.bd',
+  urgentEmail: 'urgent@aushnaracollege.edu.bd'
+}
+
+export const SOCIAL_LINKS = {
+  facebook: 'https://www.facebook.com/aushnaracollege',
+  instagram: 'https://www.instagram.com/aushnaracollege',
+  linkedin: 'https://www.linkedin.com/school/aushnaracollege'
+}
+
+export const STUDENT_COUNTS = {
+  activeEnrollmentLabel: '7,520+',
+  hscEnrollmentLabel: '2,847+',
+  lifetimeAlumniLabel: '120,000+',
+  facultyAndStaffLabel: '1,847',
+  programsLabel: '310+'
+}
+
+export const ADMISSIONS_CYCLE = {
+  label: '2026-27',
+  keywordYear: '2026',
+  applicationStart: '2025-12-01',
+  applicationDeadline: '2026-02-28',
+  documentReviewStart: '2026-03-01',
+  documentReviewEnd: '2026-03-15',
+  admissionTestStart: '2026-03-20',
+  admissionTestEnd: '2026-03-25',
+  meritListDate: '2026-04-01',
+  finalAdmissionStart: '2026-04-05',
+  finalAdmissionEnd: '2026-04-20',
+  classesBeginDate: '2026-07-01'
+}
+
